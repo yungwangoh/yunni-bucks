e9d876b64b77fdcd68d1dc1a745e765a9d59b761
swager253 <swager253@naver.com>
2023-07-24 19:18:06 +0900

    feat: 주문 관련 로직 추가

--- src/main/java/sejong/coffee/yun/domain/order/Calculator.java
+++ src/main/java/sejong/coffee/yun/domain/order/Calculator.java
@@ -1,17 +1,39 @@
 package sejong.coffee.yun.domain.order;
 
+import lombok.RequiredArgsConstructor;
+import sejong.coffee.yun.domain.discount.policy.DiscountPolicy;
+import sejong.coffee.yun.domain.exception.ExceptionControl;
+import sejong.coffee.yun.domain.exception.MenuException;
 import sejong.coffee.yun.domain.order.menu.Menu;
 import sejong.coffee.yun.domain.user.Money;
+import sejong.coffee.yun.domain.user.User;
 
+import java.math.BigDecimal;
 import java.util.List;
 
+import static sejong.coffee.yun.domain.exception.ExceptionControl.*;
+
+@RequiredArgsConstructor
 public class Calculator {
 
-    public Money calculateMenus(List<Menu> menus) {
-        Money initialPrice = Money.ZERO;
+    private final DiscountPolicy discountPolicy;
+
+    public Money calculateMenus(User user, List<Menu> menus) {
+
+        checkEmptyMenuList(menus.size());
+
+        double discount = discountPolicy.calculateDiscount(user);
+
+        Money initialPrice = Money.initialPrice(new BigDecimal(0));
 
         menus.forEach(menu -> initialPrice.plus(menu.getPrice()));
 
+        initialPrice.discount(BigDecimal.valueOf(discount));
+
         return initialPrice;
+    }
+
+    private void checkEmptyMenuList(int size) {
+        if(size <= 0) throw EMPTY_MENUS.throwException();
     }
 }
--- src/main/java/sejong/coffee/yun/domain/order/Order.java
+++ src/main/java/sejong/coffee/yun/domain/order/Order.java
@@ -1,14 +1,50 @@
 package sejong.coffee.yun.domain.order;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import sejong.coffee.yun.domain.DateTimeEntity;
+import sejong.coffee.yun.domain.order.menu.Menu;
+import sejong.coffee.yun.domain.user.User;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order extends DateTimeEntity {
 
     @Id @GeneratedValue
     private Long id;
+    @Column(name = "order_name")
+    private String name;
+    @OneToOne(fetch = FetchType.LAZY)
+    private MenuList menuList;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private Order(String name, MenuList menuList, User user) {
+        this.name = name;
+        this.menuList = menuList;
+        this.user = user;
+    }
+
+    public static Order createOrder(User user, MenuList menuList) {
+        String orderName = makeOrderName(menuList.getMenus());
+
+        return new Order(orderName, menuList, user);
+    }
+
+    private static String makeOrderName(List<Menu> menus) {
+        if(menus.size() == 0) {
+            throw new IllegalArgumentException("장바구니가 비었습니다.");
+        } else {
+            String title = menus.get(0).getTitle();
+
+            return title + " ...외" + " " + menus.size() + "개";
+        }
+    }
 }
