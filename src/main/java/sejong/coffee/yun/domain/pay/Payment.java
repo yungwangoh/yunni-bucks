182253a479b793309491acb53b835110fba4f3b1
Hayoon <hy97@sju.ac.kr>
2023-07-26 03:14:25 +0900

    feat(Payment): 결제수단 부모 클래스 구현. 결제 발생, 결제 승인 시각 공통 필드변수 생성 (@EntityListeners)

---
+++ src/main/java/sejong/coffee/yun/domain/pay/Payment.java
@@ -0,0 +1,38 @@
+package sejong.coffee.yun.domain.pay;
+
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public abstract class Payment implements Pay {
+
+    /**
+     * <결제 발생, 결제 승인>
+     * yyyy-MM-dd'T'HH:mm:ss±hh:mm ISO 8601 형식
+     */
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime requestedAt; // 결제가 일어난 날짜와 시간 정보
+
+    @Column(updatable = false)
+    private LocalDateTime approvedAt; // 결제 승인이 일어난 날짜와 시간 정보
+
+    @Override
+    public void payment() {
+
+    }
+
+    @Override
+    public void cancelPayment() {
+
+    }
+}
