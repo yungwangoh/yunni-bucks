636b7cbb7895f721dd3a61bfe508fb0c8a34f8a4
Hayoon <hy97@sju.ac.kr>
2023-07-26 03:49:55 +0900

    feat(Card): 회원 카드등록 엔티티 생성

--- src/main/java/sejong/coffee/yun/domain/user/Card.java
+++ src/main/java/sejong/coffee/yun/domain/user/Card.java
@@ -1,4 +1,24 @@
 package sejong.coffee.yun.domain.user;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of ={"id", "cardNumber", "validThru"})
 public class Card {
+
+    @Id @GeneratedValue
+    @Column(name = "card_id")
+    private Long id;
+    private String cardPhoto;
+    private String cardNumber;
+    private String validThru;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
 }
