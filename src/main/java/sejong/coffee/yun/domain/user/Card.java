7616fecd87957081d192e80e7c3835660b59f6b9
Hayoon <hy97@sju.ac.kr>
2023-07-28 01:54:03 +0900

    feat(Card): 필드변수 카드 비밀번호 추가, 생성자 추가

--- src/main/java/sejong/coffee/yun/domain/user/Card.java
+++ src/main/java/sejong/coffee/yun/domain/user/Card.java
@@ -1,6 +1,7 @@
 package sejong.coffee.yun.domain.user;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 
@@ -13,12 +14,21 @@ public class Card {
     @Id @GeneratedValue
     @Column(name = "card_id")
     private Long id;
-    private String cardPhoto;
+    @Column(length = 20)
+    @Length(max = 20)
     private String cardNumber;
+    @Column(length = 4)
+    private String cardPassword;
     private String validThru;
 
     @OneToOne
     @JoinColumn(name = "user_id")
-    private User user;
+    private Member member;
 
+    @Builder
+    public Card(String cardNumber, String validThru, String cardPassword) {
+        this.cardNumber = cardNumber;
+        this.validThru = validThru;
+        this.cardPassword = cardPassword;
+    }
 }
