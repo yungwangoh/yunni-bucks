d2dd0cabbb8de1ce780b647d8e912ea0561b4e77
Hayoon <hy97@sju.ac.kr>
2023-07-30 01:06:14 +0900

    feat(PaymentDetails): Order에 의존하는 결제내역 도메인 엔티티 생성

--- src/main/java/sejong/coffee/yun/domain/pay/PaymentDetails.java
+++ src/main/java/sejong/coffee/yun/domain/pay/PaymentDetails.java
@@ -1,21 +1,31 @@
 package sejong.coffee.yun.domain.pay;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import sejong.coffee.yun.domain.order.Order;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.*;
 
-public class PaymentDetails extends PaymentBaseEntity {
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PaymentDetails extends PaymentDateTimeEntity {
+
+    @Id @GeneratedValue
     private Long id;
     private int totalAmount; // 총 결제금액
     private int balanceAmount; // 취소할 수 있는 금액
     private int suppliedAmount; //공급가액
     private int vat; //부가세
+
+    @OneToOne
+    @JoinColumn(name = "order_id")
     private Order order;
-    private Pay pay;
 
     @Enumerated(value = EnumType.STRING)
     private PaymentType paymentType;
+
 
     /**비즈니스 로직
     * 세금 계산
