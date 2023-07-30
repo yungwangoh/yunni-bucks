580e7ba586e7dabc05a176b55225328783ffa9ac
Hayoon <hy97@sju.ac.kr>
2023-07-30 18:37:41 +0900

    test(CardPaymentTest): 카드 만료기한 유효성 테스트

--- src/test/java/sejong/coffee/yun/domain/pay/CardPaymentTest.java
+++ src/test/java/sejong/coffee/yun/domain/pay/CardPaymentTest.java
@@ -1,15 +1,19 @@
 package sejong.coffee.yun.domain.pay;
 
 import org.junit.jupiter.api.Test;
+import sejong.coffee.yun.domain.exception.CardException;
+import sejong.coffee.yun.domain.user.Card;
 
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class CardPaymentTest extends BeforeCreatedData {
 
-    /**--data '{
+    /**
+     * --data '{
      * "amount":15000,
      * "orderId":"a4CWyWY5m89PNh7xJwhk1",
      * "orderName":"토스 티셔츠 외 2건",
@@ -23,19 +27,31 @@ class CardPaymentTest extends BeforeCreatedData {
      */
 
     @Test
-    void 카드결제_단건_유효기간_검증() {
+    void 카드결제_유효기간_검증() {
         //given
 
         //when
         CardPayment cardPayment = new CardPayment(order, card);
 
         //then
-        assertThat(cardPayment.getCardExpirationYear())
-                .isGreaterThanOrEqualTo(String.valueOf(LocalDateTime.now().getDayOfYear()).substring(0, 2))
-                .isLessThanOrEqualTo("99");
+        assertThat(Integer.parseInt(cardPayment.getCardExpirationYear()))
+                .isGreaterThanOrEqualTo((LocalDateTime.now().getDayOfYear()) % 100)
+                .isLessThanOrEqualTo(99);
 
         assertThat(cardPayment.getCardExpirationMonth())
                 .isGreaterThanOrEqualTo("01")
                 .isLessThanOrEqualTo("12");
+    }
+
+    @Test
+    void 카드결제_유효기간_검증_예외발생() {
+        assertThatThrownBy(() -> Card.builder()
+                .cardNumber("123456789123")
+                .validThru("22/13")
+                .cardPassword("1234")
+                .member(member)
+                .build())
+                .isInstanceOf(CardException.class)
+                .hasMessageContaining("카드 유효기간이 올바르지 않습니다.");
     }
 }
