3ab1fea10d290fba8a0a1f93881e58ed5102e53c
swager253 <swager253@naver.com>
2023-07-30 18:38:32 +0900

    feat: 유저 예외 추가

--- src/main/java/sejong/coffee/yun/domain/exception/ExceptionControl.java
+++ src/main/java/sejong/coffee/yun/domain/exception/ExceptionControl.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ExceptionControl {
 
+    NOT_MATCH_USER("아이디 혹은 비밀번호가 다릅니다."),
     NOT_FOUND_ORDER("주문 내역이 존재하지 않습니다."),
     NOT_FOUND_USER("유저가 존재하지 않습니다."),
     EMPTY_MENUS("메뉴리스트가 비어 있습니다.");
@@ -24,5 +25,8 @@ public enum ExceptionControl {
 
     public NotFoundOrderException notFoundOrderException() {
         return new NotFoundOrderException(this.message);
+    }
+    public NotMatchUserException notMatchUserException() {
+        return new NotMatchUserException(this.message);
     }
 }
