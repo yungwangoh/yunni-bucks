ed0ff9ef65178b6605a0d232279bcefb5dbc74e1
swager253 <swager253@naver.com>
2023-07-31 17:16:02 +0900

    feat: 유저 서비스 로직 추가

--- src/main/java/sejong/coffee/yun/service/OrderService.java
+++ src/main/java/sejong/coffee/yun/service/OrderService.java
@@ -1,4 +1,39 @@
 package sejong.coffee.yun.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sejong.coffee.yun.domain.order.Calculator;
+import sejong.coffee.yun.domain.order.MenuList;
+import sejong.coffee.yun.domain.order.Order;
+import sejong.coffee.yun.domain.user.Member;
+import sejong.coffee.yun.domain.user.Money;
+import sejong.coffee.yun.repository.order.OrderRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final Calculator calculator;
+
+    @Transactional
+    public Order order(Member member, MenuList menuList) {
+
+        Money money = calculator.calculateMenus(member, menuList.getMenus());
+
+        Order order = Order.createOrder(member, menuList, money);
+
+        return orderRepository.save(order);
+    }
+
+    public Order findOrder(Long orderId) {
+        return orderRepository.findById(orderId);
+    }
+
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
 }
