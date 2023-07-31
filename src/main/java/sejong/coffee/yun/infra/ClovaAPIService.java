30474e171a6ac2148f0d395f82d09c6de46e8001
Hayoon <hy97@sju.ac.kr>
2023-08-01 02:55:16 +0900

    feat(ClovaAPIService): Ocr secret/client key 컴포넌트 생성

--- src/main/java/sejong/coffee/yun/infra/ClovaAPIService.java
+++ src/main/java/sejong/coffee/yun/infra/ClovaAPIService.java
@@ -1,9 +1,11 @@
 package sejong.coffee.yun.infra;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@Getter
 public class ClovaAPIService {
 
     private final String authorizeUri;
