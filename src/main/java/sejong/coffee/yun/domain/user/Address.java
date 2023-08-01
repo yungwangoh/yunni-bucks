fe516735580113de0b0dfd536166b0bd1b9e957c
swager253 <swager253@naver.com>
2023-08-01 18:35:13 +0900

    feat: 유저 주소에 유효성 검사 추가

--- src/main/java/sejong/coffee/yun/domain/user/Address.java
+++ src/main/java/sejong/coffee/yun/domain/user/Address.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Embeddable
 @Getter
@@ -12,8 +14,11 @@ import javax.persistence.Embeddable;
 @AllArgsConstructor
 public class Address {
 
+    @NotBlank
     private String city;
+    @NotBlank
     private String district;
     private String detail;
+    @Pattern(regexp = "^\\d{3}-\\d{3}")
     private String zipCode;
 }
