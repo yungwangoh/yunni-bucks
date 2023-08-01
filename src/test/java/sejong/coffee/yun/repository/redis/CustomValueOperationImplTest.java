c7a59dcbdc808d0923e51de8d0408efceea47b93
swager253 <swager253@naver.com>
2023-08-01 15:09:41 +0900

    test: no-sql fake 테스트

---
+++ src/test/java/sejong/coffee/yun/repository/redis/CustomValueOperationImplTest.java
@@ -0,0 +1,48 @@
+package sejong.coffee.yun.repository.redis;
+
+import org.junit.jupiter.api.Test;
+import sejong.coffee.yun.repository.redis.fake.CustomValueOperation;
+import sejong.coffee.yun.repository.redis.fake.CustomValueOperationImpl;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CustomValueOperationImplTest {
+
+    private final CustomValueOperation customValueOperation;
+
+    public CustomValueOperationImplTest() {
+        this.customValueOperation = new CustomValueOperationImpl();
+    }
+
+    @Test
+    void set_타임인_테스트() {
+        // given
+        String key = "key";
+        String value = "value";
+        customValueOperation.set(key, value, Duration.ofMillis(10000));
+
+        // when
+        String v = customValueOperation.get(key);
+
+        // then
+        assertThat(v).isEqualTo(value);
+    }
+
+    @Test
+    void set_타임아웃_테스트() throws InterruptedException{
+        // given
+        String key = "key";
+        String value = "value";
+        Duration duration = Duration.ofMillis(500);
+        customValueOperation.set(key, value, duration);
+
+        // when
+        Thread.sleep(3000);
+        String v = customValueOperation.get(key);
+
+        // then
+        assertThat(v).isNull();
+    }
+}
