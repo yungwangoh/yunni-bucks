0ea70826d0153e327756e5a039daa07c40eb0d11
swager253 <swager253@naver.com>
2023-08-02 23:35:20 +0900

    feat: custom mapper 구현

---
+++ src/main/java/sejong/coffee/yun/mapper/CustomMapper.java
@@ -0,0 +1,26 @@
+package sejong.coffee.yun.mapper;
+
+import io.jsonwebtoken.lang.Assert;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Constructor;
+
+@Component
+@Slf4j
+public class CustomMapper {
+
+    public <T> T map(Object entity, Class<T> type) {
+
+        Assert.notNull(entity, "entity must not be null!!");
+        Assert.notNull(type, "type must not be null!!");
+
+        try {
+            Constructor<T> constructor = type.getDeclaredConstructor(entity.getClass());
+
+            return constructor.newInstance(entity);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
