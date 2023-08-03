c3944c5407b4881e99d2a868dcaca64f85abd453
swager253 <swager253@naver.com>
2023-08-03 18:07:31 +0900

    fix: bean name 중복으로 인한 인터페이스 삭제

--- src/main/java/sejong/coffee/yun/repository/pay/JpaPayRepository.java
+++
@@ -1,7 +0,0 @@
-package sejong.coffee.yun.repository.pay;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sejong.coffee.yun.domain.pay.CardPayment;
-
-public interface JpaPayRepository extends JpaRepository<CardPayment, Long> {
-}
