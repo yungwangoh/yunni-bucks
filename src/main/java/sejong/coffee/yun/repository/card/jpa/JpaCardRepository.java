bd1d7ada6c070e92779234c2c49afed1cfea2ca7
swager253 <swager253@naver.com>
2023-08-05 14:49:18 +0900

    fix: 잘못된 쿼리 주석 처리

--- src/main/java/sejong/coffee/yun/repository/card/jpa/JpaCardRepository.java
+++ src/main/java/sejong/coffee/yun/repository/card/jpa/JpaCardRepository.java
@@ -1,13 +1,11 @@
 package sejong.coffee.yun.repository.card.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import sejong.coffee.yun.domain.user.Card;
-import sejong.coffee.yun.domain.user.Member;
 
 public interface JpaCardRepository extends JpaRepository<Card, Long> {
 
     // 카드 FK로 회원 찾기
-    @Query("select m from Member m where Card.member.id =: memberId")
-    Member findMemberByCard_CardId(Long id);
+//    @Query("select m from Member m where Card.member.id =: memberId")
+//    Member findMemberByCard_CardId(Long id);
 }
