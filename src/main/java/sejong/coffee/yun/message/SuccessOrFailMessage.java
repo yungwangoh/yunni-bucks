0998ef168c403a917082a6580cb60f2b71c7183b
swager253 <swager253@naver.com>
2023-08-05 22:18:17 +0900

    feat: 성공 and 실패 메세지 추가

--- src/main/java/sejong/coffee/yun/message/SuccessOrFailMessage.java
+++ src/main/java/sejong/coffee/yun/message/SuccessOrFailMessage.java
@@ -5,8 +5,14 @@ import lombok.Getter;
 @Getter
 public enum SuccessOrFailMessage {
 
+    // success
     SUCCESS_DUPLICATE_NAME("이름 중복 체크에 성공하였습니다."),
     SUCCESS_DUPLICATE_EMAIL("이메일 중복 체크에 성공하였습니다."),
+    SUCCESS_SIGN_OUT("로그아웃에 성공하였습니다."),
+    SUCCESS_SIGN_IN("로그인에 성공하였습니다."),
+    SUCCESS_DELETE_MEMBER("회원 탈퇴에 성공하였습니다."),
+
+    // fail
     ;
     private final String message;
 
