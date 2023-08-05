2fdb39666289f31cd21adc10241f211379291e74
swager253 <swager253@naver.com>
2023-08-05 14:47:17 +0900

    refactor: model mapper는 record class를 지원 안해서 custom mapper를 작성함

--- src/main/java/sejong/coffee/yun/controller/UserController.java
+++ src/main/java/sejong/coffee/yun/controller/UserController.java
@@ -1,7 +1,7 @@
 package sejong.coffee.yun.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -10,10 +10,10 @@ import sejong.coffee.yun.custom.annotation.MemberId;
 import sejong.coffee.yun.domain.order.Order;
 import sejong.coffee.yun.domain.user.Member;
 import sejong.coffee.yun.dto.user.UserDto;
+import sejong.coffee.yun.mapper.CustomMapper;
 import sejong.coffee.yun.service.UserService;
 
 import javax.validation.Valid;
-
 import java.util.List;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
@@ -22,13 +22,17 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 @RequiredArgsConstructor
 @RequestMapping("/api/members")
 @Validated
+@Slf4j
 public class UserController {
 
     private final UserService userService;
-    private final ModelMapper modelMapper;
+    private final CustomMapper customMapper;
 
     @PostMapping("")
     ResponseEntity<UserDto.Sign.Up.Response> signUp(@RequestBody @Valid UserDto.Sign.Up.Request request) {
+
+        log.info("request = {}", request.email());
+
         Member member = userService.signUp(
                 request.name(),
                 request.email(),
@@ -36,7 +40,10 @@ public class UserController {
                 request.address()
         );
 
-        UserDto.Sign.Up.Response response = modelMapper.map(member, UserDto.Sign.Up.Response.class);
+        UserDto.Sign.Up.Response response = customMapper.map(member, UserDto.Sign.Up.Response.class);
+
+
+        log.info("response = {}", response);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
@@ -45,7 +52,7 @@ public class UserController {
     ResponseEntity<UserDto.Sign.In.Response> signIn(@RequestBody @Valid UserDto.Sign.In.Request request) {
         String accessToken = userService.signIn(request.email(), request.password());
 
-        UserDto.Sign.In.Response response = modelMapper.map(accessToken, UserDto.Sign.In.Response.class);
+        UserDto.Sign.In.Response response = customMapper.map(accessToken, UserDto.Sign.In.Response.class);
 
         return ResponseEntity.ok(response);
     }
@@ -61,7 +68,7 @@ public class UserController {
     ResponseEntity<UserDto.Order.Response> findOrders(@MemberId Long memberId) {
         List<Order> orders = userService.findAllByMemberId(memberId);
 
-        UserDto.Order.Response response = modelMapper.map(orders, UserDto.Order.Response.class);
+        UserDto.Order.Response response = customMapper.map(orders, UserDto.Order.Response.class);
 
         return ResponseEntity.ok(response);
     }
@@ -70,9 +77,20 @@ public class UserController {
     ResponseEntity<UserDto.Find.Response> findById(@MemberId Long memberId) {
         Member member = userService.findMember(memberId);
 
-        UserDto.Find.Response response = modelMapper.map(member, UserDto.Find.Response.class);
+        UserDto.Find.Response response = customMapper.map(member, UserDto.Find.Response.class);
 
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/list")
+    ResponseEntity<List<UserDto.Find.Response>> findAll() {
+        List<Member> members = userService.findAll();
+
+        List<UserDto.Find.Response> collect = members.stream()
+                .map(UserDto.Find.Response::new)
+                .toList();
+
+        return ResponseEntity.ok(collect);
     }
 
     @DeleteMapping("")
@@ -83,23 +101,49 @@ public class UserController {
     }
 
     @PatchMapping("/email")
-    ResponseEntity<Void> updateEmail(@MemberId Long memberId, @RequestParam("email") String email) {
-        userService.updateEmail(memberId, email);
+    ResponseEntity<UserDto.Update.Response> updateEmail(@MemberId Long memberId,
+                                                        @RequestBody @Valid UserDto.Update.Email.Request request) {
 
-        return ResponseEntity.ok().build();
+        Member member = userService.updateEmail(memberId, request.updateEmail());
+
+        UserDto.Update.Response response = customMapper.map(member, UserDto.Update.Response.class);
+
+        return ResponseEntity.ok(response);
     }
 
     @PatchMapping("/name")
-    ResponseEntity<Void> updateName(@MemberId Long memberId, @RequestParam("name") String name) {
-        userService.updateName(memberId, name);
+    ResponseEntity<UserDto.Update.Response> updateName(@MemberId Long memberId,
+                                                       @RequestBody @Valid UserDto.Update.Name.Request request) {
 
-        return ResponseEntity.ok().build();
+        Member member = userService.updateName(memberId, request.updateName());
+
+        UserDto.Update.Response response = customMapper.map(member, UserDto.Update.Response.class);
+
+        return ResponseEntity.ok(response);
     }
 
     @PatchMapping("/password")
-    ResponseEntity<Void> updatePassword(@MemberId Long memberId, @RequestParam("password") String password) {
-        userService.updatePassword(memberId, password);
+    ResponseEntity<UserDto.Update.Response> updatePassword(@MemberId Long memberId,
+                                                           @RequestBody @Valid UserDto.Update.Password.Request request) {
 
-        return ResponseEntity.ok().build();
+        Member member = userService.updatePassword(memberId, request.updatePassword());
+
+        UserDto.Update.Response response = customMapper.map(member, UserDto.Update.Response.class);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/duplication/name")
+    ResponseEntity<String> duplicateName(@RequestParam("name") String name) {
+        String s = userService.duplicateName(name);
+
+        return ResponseEntity.ok(s);
+    }
+
+    @GetMapping("/duplication/email")
+    ResponseEntity<String> duplicateEmail(@RequestParam("email") String email) {
+        String s = userService.duplicateEmail(email);
+
+        return ResponseEntity.ok(s);
     }
 }
