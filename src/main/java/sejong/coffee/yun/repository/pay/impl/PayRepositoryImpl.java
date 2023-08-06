daf306f3a35cd4a37e0f041f060cecdfa164eae5
Hayoon <hy97@sju.ac.kr>
2023-08-06 22:38:33 +0900

    feat(PayRepositoryImpl): 결제 RepositoryImpl 구현

--- src/main/java/sejong/coffee/yun/repository/pay/impl/PayRepositoryImpl.java
+++ src/main/java/sejong/coffee/yun/repository/pay/impl/PayRepositoryImpl.java
@@ -3,12 +3,12 @@ package sejong.coffee.yun.repository.pay.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import sejong.coffee.yun.domain.pay.CardPayment;
+import sejong.coffee.yun.domain.pay.PaymentStatus;
 import sejong.coffee.yun.repository.pay.PayRepository;
 import sejong.coffee.yun.repository.pay.jpa.JpaPayRepository;
 
 import java.util.List;
-
-import static sejong.coffee.yun.domain.exception.ExceptionControl.EMPTY_MENUS;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -22,13 +22,22 @@ public class PayRepositoryImpl implements PayRepository {
     }
 
     @Override
-    public CardPayment findById(Long id) {
-        return jpaPayRepository.findById(id)
-                .orElseThrow(EMPTY_MENUS::notFoundException);
+    public Optional<CardPayment> findById(Long id) {
+        return jpaPayRepository.findById(id);
     }
 
     @Override
     public List<CardPayment> findAll() {
         return jpaPayRepository.findAll();
+    }
+
+    @Override
+    public Optional<CardPayment> findByOrderId(String orderId) {
+        return jpaPayRepository.findByOrderId(orderId);
+    }
+
+    @Override
+    public Optional<CardPayment> findByPaymentKeyAndPaymentStatus(String paymentKey, PaymentStatus paymentStatus) {
+        return jpaPayRepository.findByPaymentKeyAndPaymentStatus(paymentKey, paymentStatus);
     }
 }
