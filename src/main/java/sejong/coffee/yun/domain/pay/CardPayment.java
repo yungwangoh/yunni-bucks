ab9fb67da030811ca46cfec9d78a541fb0585dfd
Hayoon <hy97@sju.ac.kr>
2023-08-06 22:27:01 +0900

    feat(CardPayment): 필드변수 추가, @Builder 추가

--- src/main/java/sejong/coffee/yun/domain/pay/CardPayment.java
+++ src/main/java/sejong/coffee/yun/domain/pay/CardPayment.java
@@ -1,20 +1,19 @@
 package sejong.coffee.yun.domain.pay;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import sejong.coffee.yun.domain.order.Order;
 import sejong.coffee.yun.domain.user.Card;
 
 import javax.persistence.*;
 
+import static sejong.coffee.yun.domain.pay.PaymentStatus.READY;
 import static sejong.coffee.yun.util.parse.ParsingDateTimeUtil.parsingCardValidDate;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of ={"id", "cardNumber", "cardPassword"})
+//@JsonIgnoreProperties(ignoreUnknown = true)
 public class CardPayment extends PaymentDateTimeEntity implements Pay {
 
     @Id @GeneratedValue
@@ -25,18 +24,23 @@ public class CardPayment extends PaymentDateTimeEntity implements Pay {
     private String customerName;
     private String cardExpirationYear;
     private String cardExpirationMonth;
+    private String paymentKey;
+    private PaymentType paymentType;
+    private PaymentStatus paymentStatus;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
-    public CardPayment(Order order, Card card) {
-        this.cardNumber = card.getCardNumber();
+    @Builder
+    public CardPayment(Card card, Order order) {
+        this.cardNumber = card.getNumber();
         this.cardPassword = displayTwoDigits(card.getCardPassword());
         this.customerName = order.getMember().getName();
         this.cardExpirationYear = parsingCardValidDate(card.getValidThru())[0];
         this.cardExpirationMonth = parsingCardValidDate(card.getValidThru())[1];
         this.order = order;
+        this.paymentStatus = READY;
     }
 
     @Override
@@ -51,5 +55,10 @@ public class CardPayment extends PaymentDateTimeEntity implements Pay {
 
     public String displayTwoDigits(String carPassword) {
         return carPassword.substring(0, 2);
+    }
+
+
+    public void update() {
+        this.paymentStatus = PaymentStatus.DONE;
     }
 }
