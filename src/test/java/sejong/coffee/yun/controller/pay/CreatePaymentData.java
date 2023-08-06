abe796f05d6830819ae87f0435c261b0cbfedae8
Hayoon <hy97@sju.ac.kr>
2023-08-06 22:27:50 +0900

    feat: CardPayment 테스트 Data 추가

---
+++ src/test/java/sejong/coffee/yun/controller/pay/CreatePaymentData.java
@@ -0,0 +1,23 @@
+package sejong.coffee.yun.controller.pay;
+
+import sejong.coffee.yun.domain.order.Order;
+import sejong.coffee.yun.domain.pay.BeforeCreatedData;
+import sejong.coffee.yun.domain.pay.CardPayment;
+import sejong.coffee.yun.domain.user.Card;
+
+class CreatePaymentData extends BeforeCreatedData {
+
+    protected final CardPayment cardPayment;
+
+    private Order getOrderByParentData() {
+        return order;
+    }
+
+    public Card getCardByParentData() {
+        return card;
+    }
+
+    public CreatePaymentData() {
+        this.cardPayment = new CardPayment(getCardByParentData(), getOrderByParentData());
+    }
+}
