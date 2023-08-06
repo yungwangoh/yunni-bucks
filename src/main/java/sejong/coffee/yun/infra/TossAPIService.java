7423ec5939855ebebca55457e4ff626aa6b1a0bc
Hayoon <hy97@sju.ac.kr>
2023-08-06 22:32:14 +0900

    feat(TossAPIService): Toss api 서비스 분리

--- src/main/java/sejong/coffee/yun/infra/TossAPIService.java
+++ src/main/java/sejong/coffee/yun/infra/TossAPIService.java
@@ -1,11 +1,20 @@
 package sejong.coffee.yun.infra;
 
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import sejong.coffee.yun.domain.pay.CardPayment;
+import sejong.coffee.yun.dto.CardPaymentDto;
 
-@Component
-@Getter
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static sejong.coffee.yun.util.parse.JsonParsing.parsePaymentObjectByJson;
+import static sejong.coffee.yun.util.parse.JsonParsing.parsePaymentStringByJson;
+
+@Service
 public class TossAPIService {
 
     private final String apiUri;
@@ -16,5 +25,18 @@ public class TossAPIService {
 
         this.apiUri = apiUri;
         this.secretKey = secretKey;
+    }
+
+    public CardPayment callExternalAPI(CardPaymentDto.Request cardPaymentDto) throws IOException, InterruptedException {
+        // 외부 API 호출하는 로직
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(apiUri))
+                .header("Authorization", secretKey)
+                .header("Content-Type", "application/json")
+                .method("POST", HttpRequest.BodyPublishers.ofString(parsePaymentStringByJson(cardPaymentDto)))
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        // 예외처리(토스 커스텀 -> 내부 프로젝트 커스텀) TODO
+        return parsePaymentObjectByJson(response.body());
     }
 }
