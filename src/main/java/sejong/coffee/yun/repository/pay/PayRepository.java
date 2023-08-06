5621d32451bdee78e39d254ee0613ecf5292dc82
Hayoon <hy97@sju.ac.kr>
2023-08-06 22:31:06 +0900

    feat(PayRepository): 결제단건 조회 추가

--- src/main/java/sejong/coffee/yun/repository/pay/PayRepository.java
+++ src/main/java/sejong/coffee/yun/repository/pay/PayRepository.java
@@ -1,15 +1,20 @@
 package sejong.coffee.yun.repository.pay;
 
 import sejong.coffee.yun.domain.pay.CardPayment;
+import sejong.coffee.yun.domain.pay.PaymentStatus;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PayRepository {
 
     CardPayment save(CardPayment cardPayment);
 
-    CardPayment findById(Long id);
+    Optional<CardPayment> findById(Long id);
 
     List<CardPayment> findAll();
+
+    Optional<CardPayment> findByOrderId(String orderId);
+    Optional<CardPayment> findByPaymentKeyAndStatus(String paymentKey, PaymentStatus paymentStatus);
 
 }
