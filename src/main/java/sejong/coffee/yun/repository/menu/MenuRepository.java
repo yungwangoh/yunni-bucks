dec039e58ff561f03011561fdd39ac15908325aa
swager253 <swager253@naver.com>
2023-08-08 18:02:49 +0900

    feat: MenuRepository 추가

--- src/main/java/sejong/coffee/yun/repository/menu/MenuRepository.java
+++ src/main/java/sejong/coffee/yun/repository/menu/MenuRepository.java
@@ -1,4 +1,13 @@
 package sejong.coffee.yun.repository.menu;
 
+import sejong.coffee.yun.domain.order.menu.Menu;
+
+import java.util.List;
+
 public interface MenuRepository {
+
+    Menu save(Menu menu);
+    Menu findById(Long id);
+    List<Menu> findAll();
+    void delete(Long id);
 }
