5495935aef3111916f50b87f1eea8d9ec742f765
Hayoon <hy97@sju.ac.kr>
2023-08-09 22:13:34 +0900

    feat(PayService): PayService paymentDto 및 payment 데이터 가공 메서드

--- src/main/java/sejong/coffee/yun/service/PayService.java
+++ src/main/java/sejong/coffee/yun/service/PayService.java
@@ -1,31 +1,41 @@
 package sejong.coffee.yun.service;
 
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sejong.coffee.yun.domain.exception.ExceptionControl;
+import sejong.coffee.yun.domain.order.Order;
 import sejong.coffee.yun.domain.pay.CardPayment;
+import sejong.coffee.yun.domain.user.Card;
 import sejong.coffee.yun.dto.CardPaymentDto;
-import sejong.coffee.yun.infra.TossAPIService;
+import sejong.coffee.yun.infra.TossAPIServiceImpl;
+import sejong.coffee.yun.infra.port.UuidHolder;
 import sejong.coffee.yun.repository.pay.PayRepository;
 
 import java.io.IOException;
 import java.util.Optional;
 
 import static sejong.coffee.yun.domain.pay.PaymentStatus.DONE;
+import static sejong.coffee.yun.dto.CardPaymentDto.Response;
 
 @Service
 @RequiredArgsConstructor
+@Builder
 public class PayService {
+
     private final PayRepository payRepository;
-    private final TossAPIService tossAPIService;
+    private final TossAPIServiceImpl tossAPIServiceImpl;
+    private final OrderService orderService;
+    private final CardService cardService;
+    private final UuidHolder uuidHolder;
 
     public Optional<CardPayment> findById(long id) {
         return payRepository.findById(id);
     }
 
-    public CardPayment getByOrderId(String orderId) {
-        return payRepository.findByOrderId(orderId)
+    public CardPayment getByOrderId(String orderUuid) {
+        return payRepository.findByOrderIdAnAndPaymentStatus(orderUuid, DONE)
                 .orElseThrow(ExceptionControl.NOT_FOUND_PAY_DETAILS::paymentDetailsException);
     }
 
@@ -35,30 +45,19 @@ public class PayService {
     }
 
     @Transactional
-    public CardPayment create(CardPayment cardPayment) throws IOException, InterruptedException {
-        CardPaymentDto.Request request = CardPaymentDto.Request.from(cardPayment);
-        CardPayment result = tossAPIService.callExternalAPI(request);
-        result = payRepository.save(result);
-        return result;
+    public CardPayment pay(CardPaymentDto.Request request) throws IOException, InterruptedException {
+
+        Response response = tossAPIServiceImpl.callExternalAPI(request);
+        CardPayment approvalPayment = CardPayment.approvalPayment(CardPayment.fromModel(request), response.paymentKey(), response.approvedAt());
+        approvalPayment = payRepository.save(approvalPayment);
+        return approvalPayment;
     }
 
     @Transactional
-    public void approvalPayment(String paymentKey, String orderId) {
-        CardPayment cardPayment = payRepository.findByOrderId(orderId)
-                .orElseThrow(ExceptionControl.NOT_FOUND_PAY_DETAILS::paymentDetailsException);
-        if (!paymentKey.equals(cardPayment.getPaymentKey())) {
-            throw ExceptionControl.NOT_MATCHED_PAYMENT_KEY.paymentDetailsException();
-        }
-        update(paymentKey, orderId);
-        payRepository.save(cardPayment); // 결제승인
-    }
+    public CardPaymentDto.Request initPayment(Long orderId){
+        Order order = orderService.findOrder(orderId);
+        Card card = cardService.getByMemberId(order.getMember().getId());
 
-    @Transactional
-    public void update(String paymentKey, String orderId) {
-        CardPayment findCardPayment = getByOrderId(orderId);
-        if (!findCardPayment.getPaymentKey().equals(paymentKey)) {
-            throw ExceptionControl.NOT_MATCHED_PAYMENT_KEY.paymentDetailsException();
-        }
-        findCardPayment.update();
+        return CardPaymentDto.Request.create(card, order, uuidHolder);
     }
 }
