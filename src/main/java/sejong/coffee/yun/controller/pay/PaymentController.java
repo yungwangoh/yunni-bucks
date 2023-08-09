fd02ff85ac48bf553bdf33ac930222f35b9242b8
Hayoon <hy97@sju.ac.kr>
2023-08-09 22:14:01 +0900

    feat(PaymentController): PaymentController 구현

--- src/main/java/sejong/coffee/yun/controller/pay/PaymentController.java
+++ src/main/java/sejong/coffee/yun/controller/pay/PaymentController.java
@@ -1,28 +1,49 @@
 package sejong.coffee.yun.controller.pay;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sejong.coffee.yun.domain.pay.CardPayment;
-import sejong.coffee.yun.dto.CardPaymentDto;
-import sejong.coffee.yun.infra.TossAPIService;
+import sejong.coffee.yun.mapper.CustomMapper;
+import sejong.coffee.yun.service.PayService;
 
 import java.io.IOException;
 
+import static sejong.coffee.yun.dto.CardPaymentDto.Request;
+import static sejong.coffee.yun.dto.CardPaymentDto.Response;
+
 @RestController
-@RequestMapping("/pay")
+@RequestMapping("/api/payments/")
 @RequiredArgsConstructor
+@Slf4j
 public class PaymentController {
 
-    private final TossAPIService tossAPIService;
+    private final PayService payService;
+    private final CustomMapper customMapper;
 
-    @PostMapping("/v1/payments/card")
-    public ResponseEntity<CardPayment> keyIn(@RequestBody CardPaymentDto.Request cardPaymentDto) throws IOException, InterruptedException {
-        CardPayment cardPayment = tossAPIService.callExternalAPI(cardPaymentDto);
-        return new ResponseEntity<>(cardPayment, HttpStatus.OK);
+    @PostMapping("/card-payment/{orderId}")
+    public ResponseEntity<Response> keyIn(
+            @PathVariable Long orderId) throws IOException, InterruptedException {
+        Request request = payService.initPayment(orderId);
+        CardPayment cardPayment = payService.pay(request);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(customMapper.map(cardPayment, Response.class));
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<Response> getByOrderId(@PathVariable String orderId) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(customMapper.map(payService.getByOrderId(orderId), Response.class));
+    }
+
+    @GetMapping("/{paymentKey}")
+    public ResponseEntity<Response> getByPaymentKey(@PathVariable String paymentKey) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(customMapper.map(payService.getByPaymentKey(paymentKey), Response.class));
     }
 }
