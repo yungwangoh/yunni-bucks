77454409521175c3ac285c2ec1a53155e72163d0
Hayoon <hy97@sju.ac.kr>
2023-08-09 22:11:40 +0900

    feat(CardRepository): CardRepository 의존성역전 구현

--- src/main/java/sejong/coffee/yun/repository/card/CardRepository.java
+++ src/main/java/sejong/coffee/yun/repository/card/CardRepository.java
@@ -3,12 +3,14 @@ package sejong.coffee.yun.repository.card;
 import sejong.coffee.yun.domain.user.Card;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CardRepository {
 
     Card save(Card card);
 
-    Card findById(Long id);
+    Optional<Card> findById(Long id);
+    Optional<Card> findByMemberId(Long memberId);
 
     List<Card> findAll();
 }
--- src/main/java/sejong/coffee/yun/repository/card/impl/CardRepositoryImpl.java
+++ src/main/java/sejong/coffee/yun/repository/card/impl/CardRepositoryImpl.java
@@ -2,18 +2,18 @@ package sejong.coffee.yun.repository.card.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-import sejong.coffee.yun.domain.exception.ExceptionControl;
 import sejong.coffee.yun.domain.user.Card;
 import sejong.coffee.yun.repository.card.CardRepository;
 import sejong.coffee.yun.repository.card.jpa.JpaCardRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class CardRepositoryImpl implements CardRepository {
 
-    private JpaCardRepository jpaCardRepository;
+    private final JpaCardRepository jpaCardRepository;
 
     @Override
     public Card save(Card card) {
@@ -21,9 +21,13 @@ public class CardRepositoryImpl implements CardRepository {
     }
 
     @Override
-    public Card findById(Long id) {
-        return jpaCardRepository.findById(id)
-                .orElseThrow(ExceptionControl.NOT_FOUND_REGISTER_CARD::cardException);
+    public Optional<Card> findById(Long id) {
+        return jpaCardRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Card> findByMemberId(Long memberId) {
+        return jpaCardRepository.findById(memberId);
     }
 
     @Override
