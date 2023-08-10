67db36de081f889201fe71b27f7a4a34436b8c9c
Hayoon <hy97@sju.ac.kr>
2023-08-10 20:18:46 +0900

    feat(ApiService): 외부 API 호출을 위한 DIP 클래스 분리

---
+++ src/main/java/sejong/coffee/yun/infra/ApiService.java
@@ -0,0 +1,21 @@
+package sejong.coffee.yun.infra;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sejong.coffee.yun.infra.port.TossApiService;
+
+import java.io.IOException;
+
+import static sejong.coffee.yun.dto.CardPaymentDto.Request;
+import static sejong.coffee.yun.dto.CardPaymentDto.Response;
+
+@Service
+@RequiredArgsConstructor
+public class ApiService {
+
+    private final TossApiService tossApiService;
+
+    public Response callApi(Request request) throws IOException, InterruptedException {
+        return tossApiService.callExternalAPI(request);
+    }
+}
