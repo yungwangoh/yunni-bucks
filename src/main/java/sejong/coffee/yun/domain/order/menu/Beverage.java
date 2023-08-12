899bc1e15c7993e6b82fb473e1364b5823402464
swager253 <swager253@naver.com>
2023-08-12 17:49:24 +0900

    feat: 시간 로직 추가

--- src/main/java/sejong/coffee/yun/domain/order/menu/Beverage.java
+++ src/main/java/sejong/coffee/yun/domain/order/menu/Beverage.java
@@ -2,10 +2,15 @@ package sejong.coffee.yun.domain.order.menu;
 
 import sejong.coffee.yun.domain.user.Money;
 
+import java.time.LocalDateTime;
+
 public class Beverage extends Menu {
 
-    public Beverage(final String title, final String description, final Money price, final Nutrients nutrients, final MenuSize menuSize) {
-        super(title, description, price, nutrients, menuSize);
+    public Beverage(final String title, final String description,
+                    final Money price, final Nutrients nutrients,
+                    final MenuSize menuSize, final LocalDateTime now) {
+
+        super(title, description, price, nutrients, menuSize, now);
     }
 }
 
--- src/main/java/sejong/coffee/yun/domain/order/menu/Bread.java
+++ src/main/java/sejong/coffee/yun/domain/order/menu/Bread.java
@@ -2,9 +2,14 @@ package sejong.coffee.yun.domain.order.menu;
 
 import sejong.coffee.yun.domain.user.Money;
 
+import java.time.LocalDateTime;
+
 public class Bread extends Menu {
 
-    public Bread(final String title, final String description, final Money price, final Nutrients nutrients, final MenuSize menuSize) {
-        super(title, description, price, nutrients, menuSize);
+    public Bread(final String title, final String description,
+                 final Money price, final Nutrients nutrients,
+                 final MenuSize menuSize, LocalDateTime now) {
+
+        super(title, description, price, nutrients, menuSize, now);
     }
 }
