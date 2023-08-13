88cc6e9af3ef625f8fe2f38f0bbcc9967b57defd
Hayoon <hy97@sju.ac.kr>
2023-08-13 18:17:28 +0900

    style: directory 변경

--- src/test/java/sejong/coffee/yun/service/externalApi/TossApiServiceTest.java
+++ src/test/java/sejong/coffee/yun/service/externalApi/TossApiServiceTest.java
@@ -3,14 +3,14 @@ package sejong.coffee.yun.service.externalApi;
 import org.junit.jupiter.api.Test;
 import sejong.coffee.yun.controller.pay.CreatePaymentData;
 import sejong.coffee.yun.domain.pay.PaymentStatus;
-import sejong.coffee.yun.dto.CardPaymentDto;
+import sejong.coffee.yun.dto.pay.CardPaymentDto;
 import sejong.coffee.yun.infra.ApiService;
 import sejong.coffee.yun.infra.fake.FakeApiService;
 
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static sejong.coffee.yun.dto.CardPaymentDto.Request;
+import static sejong.coffee.yun.dto.pay.CardPaymentDto.Request;
 
 public class TossApiServiceTest extends CreatePaymentData {
 
@@ -26,7 +26,7 @@ public class TossApiServiceTest extends CreatePaymentData {
 
         //then
         assertThat(response.orderName()).isEqualTo("커피 외 3개");
-        assertThat(response.totalAmount()).isEqualTo("3000.0");
+        assertThat(response.totalAmount()).isEqualTo("3000");
         assertThat(response.cardNumber()).containsAnyOf("12341234");
         assertThat(response.paymentStatus()).isEqualTo(PaymentStatus.DONE);
         assertThat(response.orderUuid()).isEqualTo("asdfasdf");
