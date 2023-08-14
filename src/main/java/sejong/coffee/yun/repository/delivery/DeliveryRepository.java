360782135b478d9891608d7004f750fe1c4b7312
swager253 <swager253@naver.com>
2023-08-14 22:43:30 +0900

    feat: 배달 타입에 따른 페이징 조회 추가

--- src/main/java/sejong/coffee/yun/repository/delivery/DeliveryRepository.java
+++ src/main/java/sejong/coffee/yun/repository/delivery/DeliveryRepository.java
@@ -3,10 +3,17 @@ package sejong.coffee.yun.repository.delivery;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import sejong.coffee.yun.domain.delivery.Delivery;
+import sejong.coffee.yun.domain.delivery.DeliveryStatus;
+import sejong.coffee.yun.domain.delivery.DeliveryType;
+
+import java.util.List;
 
 public interface DeliveryRepository {
 
     Delivery save(Delivery delivery);
+    Delivery findOne(Long deliveryId);
+    List<Delivery> findAll();
     Page<Delivery> findByMemberId(Pageable pageable, Long memberId);
-    Page<Delivery> findDeliveryStatusByMemberId(Pageable pageable, Long memberId);
+    Page<Delivery> findDeliveryTypeByMemberId(Pageable pageable, Long memberId, DeliveryType type);
+    Page<Delivery> findDeliveryStatusByMemberId(Pageable pageable, Long memberId, DeliveryStatus status);
 }
--- src/main/java/sejong/coffee/yun/repository/delivery/impl/DeliveryRepositoryImpl.java
+++ src/main/java/sejong/coffee/yun/repository/delivery/impl/DeliveryRepositoryImpl.java
@@ -10,12 +10,17 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import sejong.coffee.yun.domain.delivery.Delivery;
 import sejong.coffee.yun.domain.delivery.DeliveryStatus;
+import sejong.coffee.yun.domain.delivery.DeliveryType;
+import sejong.coffee.yun.domain.delivery.ReserveDelivery;
 import sejong.coffee.yun.repository.delivery.DeliveryRepository;
 import sejong.coffee.yun.repository.delivery.jpa.JpaDeliveryRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static sejong.coffee.yun.domain.delivery.QDelivery.delivery;
+import static sejong.coffee.yun.domain.delivery.QReserveDelivery.reserveDelivery;
+import static sejong.coffee.yun.domain.exception.ExceptionControl.NOT_FOUND_DELIVERY;
 
 @Repository
 @RequiredArgsConstructor
@@ -28,6 +33,22 @@ public class DeliveryRepositoryImpl implements DeliveryRepository {
     @Transactional
     public Delivery save(Delivery delivery) {
         return jpaDeliveryRepository.save(delivery);
+    }
+
+    @Override
+    public Delivery findOne(Long deliveryId) {
+        return jpaDeliveryRepository.findById(deliveryId)
+                .orElseThrow(NOT_FOUND_DELIVERY::notFoundException);
+    }
+
+    @Override
+    public List<Delivery> findAll() {
+        List<ReserveDelivery> deliveryList = jpaQueryFactory.selectFrom(reserveDelivery)
+                .where(reserveDelivery.status.eq(DeliveryStatus.READY))
+                .orderBy(reserveDelivery.reserveAt.asc(), reserveDelivery.createAt.asc())
+                .fetch();
+
+        return new ArrayList<>(deliveryList);
     }
 
     @Override
@@ -46,10 +67,26 @@ public class DeliveryRepositoryImpl implements DeliveryRepository {
     }
 
     @Override
-    public Page<Delivery> findDeliveryStatusByMemberId(Pageable pageable, Long memberId) {
+    public Page<Delivery> findDeliveryTypeByMemberId(Pageable pageable, Long memberId, DeliveryType type) {
         List<Delivery> deliveries = jpaQueryFactory.selectFrom(delivery)
                 .where(delivery.order.member.id.eq(memberId)).fetchJoin()
-                .where(delivery.status.eq(DeliveryStatus.RESERVE))
+                .where(delivery.type.eq(type))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .orderBy(delivery.createAt.desc())
+                .fetch();
+
+        JPAQuery<Long> jpaQuery = jpaQueryFactory.select(delivery.count())
+                .from(delivery);
+
+        return PageableExecutionUtils.getPage(deliveries, pageable, jpaQuery::fetchOne);
+    }
+
+    @Override
+    public Page<Delivery> findDeliveryStatusByMemberId(Pageable pageable, Long memberId, DeliveryStatus status) {
+        List<Delivery> deliveries = jpaQueryFactory.selectFrom(delivery)
+                .where(delivery.order.member.id.eq(memberId)).fetchJoin()
+                .where(delivery.status.eq(status))
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .orderBy(delivery.createAt.desc())
