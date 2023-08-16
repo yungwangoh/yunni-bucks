0c67e85757cb68b8ed707ab21fb3d15e65208f54
Hayoon <hy97@sju.ac.kr>
2023-08-16 21:44:19 +0900

    feat(TestPayContainer): 테스트용 fake container 생성

---
+++ src/test/java/sejong/coffee/yun/controller/pay/mock/TestPayContainer.java
@@ -0,0 +1,45 @@
+package sejong.coffee.yun.controller.pay.mock;
+
+import lombok.Builder;
+import sejong.coffee.yun.infra.ApiService;
+import sejong.coffee.yun.infra.fake.FakeApiService;
+import sejong.coffee.yun.infra.fake.FakeUuidHolder;
+import sejong.coffee.yun.infra.port.UuidHolder;
+import sejong.coffee.yun.repository.card.CardRepository;
+import sejong.coffee.yun.repository.card.fake.FakeCardRepository;
+import sejong.coffee.yun.repository.order.OrderRepository;
+import sejong.coffee.yun.repository.order.fake.FakeOrderRepository;
+import sejong.coffee.yun.repository.pay.PayRepository;
+import sejong.coffee.yun.repository.pay.fake.FakePayRepository;
+import sejong.coffee.yun.repository.user.UserRepository;
+import sejong.coffee.yun.repository.user.fake.FakeUserRepository;
+import sejong.coffee.yun.service.CardService;
+import sejong.coffee.yun.service.PayService;
+
+public class TestPayContainer {
+
+    public final PayRepository payRepository;
+    public final OrderRepository orderRepository;
+    public final CardRepository cardRepository;
+    public final UuidHolder uuidHolder;
+    public final PayService payService;
+    public final ApiService apiService;
+    public final CardService cardService;
+    public final UserRepository userRepository;
+
+    @Builder
+    public TestPayContainer(String uuid, String paymentKey) {
+        this.payRepository = new FakePayRepository();
+        this.orderRepository = new FakeOrderRepository();
+        this.cardRepository = new FakeCardRepository();
+        this.userRepository = new FakeUserRepository();
+        this.uuidHolder = new FakeUuidHolder(uuid);
+        this.apiService = new ApiService(new FakeApiService(paymentKey));
+        this.payService = new PayService(
+                this.apiService, this.payRepository, this.orderRepository
+                ,this.cardRepository, this.uuidHolder);
+        cardService = new CardService(
+                this.cardRepository,
+                this.userRepository);
+    }
+}
