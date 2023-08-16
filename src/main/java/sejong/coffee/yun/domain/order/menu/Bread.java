f8996158a943aa8637df322cb3d6e38c64ca8ffc
swager253 <swager253@naver.com>
2023-08-16 23:01:26 +0900

    refactor: final 추가

--- src/main/java/sejong/coffee/yun/domain/order/menu/Bread.java
+++ src/main/java/sejong/coffee/yun/domain/order/menu/Bread.java
@@ -8,7 +8,7 @@ public class Bread extends Menu {
 
     public Bread(final String title, final String description,
                  final Money price, final Nutrients nutrients,
-                 final MenuSize menuSize, LocalDateTime now) {
+                 final MenuSize menuSize, final LocalDateTime now) {
 
         super(title, description, price, nutrients, menuSize, now);
     }
