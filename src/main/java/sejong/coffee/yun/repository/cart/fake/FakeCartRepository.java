1e9a5f8896e853ac0c636c98c59a9555b97dbbc2
swager253 <swager253@naver.com>
2023-08-17 23:54:06 +0900

    feat: fake repo

--- src/main/java/sejong/coffee/yun/repository/cart/fake/FakeCartRepository.java
+++ src/main/java/sejong/coffee/yun/repository/cart/fake/FakeCartRepository.java
@@ -1,40 +1,59 @@
 package sejong.coffee.yun.repository.cart.fake;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import sejong.coffee.yun.domain.user.Cart;
 import sejong.coffee.yun.repository.cart.CartRepository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
+import static sejong.coffee.yun.domain.exception.ExceptionControl.NOT_FOUND_CART;
+
 
 @Repository
-@RequiredArgsConstructor
 public class FakeCartRepository implements CartRepository {
 
-    private final List<Cart> carts;
+    private final List<Cart> carts = new ArrayList<>();
+    private Long id = 0L;
 
     @Override
     public Cart save(Cart cart) {
-        return null;
+        Cart newCart = Cart.from(++id, cart);
+
+        carts.add(newCart);
+
+        return newCart;
     }
 
     @Override
     public Cart findById(Long id) {
-        return null;
+        return carts.stream()
+                .filter(cart -> Objects.equals(cart.getId(), id))
+                .findAny()
+                .orElseThrow(NOT_FOUND_CART::notFoundException);
     }
 
     @Override
     public Cart findByMember(Long memberId) {
-        return null;
+        return carts.stream()
+                .filter(cart -> Objects.equals(cart.getMember().getId(), memberId))
+                .findAny()
+                .orElseThrow(NOT_FOUND_CART::notFoundException);
     }
 
     @Override
     public void delete(Long id) {
+        Cart findCart = carts.stream()
+                .filter(cart -> Objects.equals(cart.getId(), id))
+                .findAny()
+                .orElseThrow(NOT_FOUND_CART::notFoundException);
 
+        carts.remove(findCart);
     }
 
     @Override
     public void delete(Cart cart) {
-
+        carts.remove(cart);
     }
 }
