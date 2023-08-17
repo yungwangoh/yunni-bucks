b40b6302552db1c6b7e68ff1125e0bca2983a994
swager253 <swager253@naver.com>
2023-08-17 23:51:13 +0900

    feat: fake 정적 팩토리 메서드 추가

--- src/main/java/sejong/coffee/yun/domain/order/menu/Beverage.java
+++ src/main/java/sejong/coffee/yun/domain/order/menu/Beverage.java
@@ -1,9 +1,18 @@
 package sejong.coffee.yun.domain.order.menu;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import sejong.coffee.yun.domain.user.Money;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 import java.time.LocalDateTime;
 
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@DiscriminatorValue("Beverage")
 public class Beverage extends Menu {
 
     public Beverage(final String title, final String description,
@@ -11,6 +20,14 @@ public class Beverage extends Menu {
                     final MenuSize menuSize, final LocalDateTime now) {
 
         super(title, description, price, nutrients, menuSize, now);
+    }
+
+    private Beverage(Long id, String title, String description, Money price, Nutrients nutrients, MenuSize menuSize, LocalDateTime now) {
+        super(id, title, description, price, nutrients, menuSize, now, now);
+    }
+
+    public static Beverage from(Long id, Beverage beverage) {
+        return new Beverage(id, beverage.getTitle(), beverage.getDescription(), beverage.getPrice(), beverage.getNutrients(), beverage.getMenuSize(), beverage.getCreateAt());
     }
 }
 
--- src/main/java/sejong/coffee/yun/domain/order/menu/Bread.java
+++ src/main/java/sejong/coffee/yun/domain/order/menu/Bread.java
@@ -1,9 +1,18 @@
 package sejong.coffee.yun.domain.order.menu;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import sejong.coffee.yun.domain.user.Money;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 import java.time.LocalDateTime;
 
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@DiscriminatorValue("Bread")
 public class Bread extends Menu {
 
     public Bread(final String title, final String description,
@@ -11,5 +20,13 @@ public class Bread extends Menu {
                  final MenuSize menuSize, final LocalDateTime now) {
 
         super(title, description, price, nutrients, menuSize, now);
+    }
+
+    private Bread(Long id, String title, String description, Money price, Nutrients nutrients, MenuSize menuSize, LocalDateTime now) {
+        super(id, title, description, price, nutrients, menuSize, now, now);
+    }
+
+    public static Bread from(Long id, Bread bread) {
+        return new Bread(id, bread.getTitle(), bread.getDescription(), bread.getPrice(), bread.getNutrients(), bread.getMenuSize(), bread.getCreateAt());
     }
 }
