906dcb9d97a441b038a745cff542aa8e1d095ccd
Hayoon <hy97@sju.ac.kr>
2023-08-19 21:18:56 +0900

    test(PaymentControllerTest): 결제 취소 api 테스트

--- src/test/java/sejong/coffee/yun/controller/pay/PaymentControllerTest.java
+++ src/test/java/sejong/coffee/yun/controller/pay/PaymentControllerTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import sejong.coffee.yun.controller.pay.mock.TestPayContainer;
+import sejong.coffee.yun.domain.exception.ExceptionControl;
 import sejong.coffee.yun.domain.order.Order;
+import sejong.coffee.yun.domain.pay.PaymentCancelReason;
 import sejong.coffee.yun.domain.pay.PaymentStatus;
 import sejong.coffee.yun.domain.user.Member;
 import sejong.coffee.yun.dto.card.CardDto;
@@ -16,6 +18,7 @@ import java.time.LocalDateTime;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PaymentControllerTest extends CreatePaymentData {
 
@@ -115,5 +118,52 @@ class PaymentControllerTest extends CreatePaymentData {
         assertThat(result.getBody().cardExpirationMonth()).isEqualTo("10");
         assertThat(result.getBody().paymentKey()).isEqualTo("testPaymentKey");
         assertThat(result.getBody().cardNumber()).isEqualTo(this.card.getNumber());
+    }
+
+    @Test
+    public void cancelPaymentKey로_결제를_취소한다() throws Exception {
+
+        // given
+        PaymentController.builder()
+                .payService(testPayContainer.payService)
+                .customMapper(new CustomMapper())
+                .build()
+                .keyIn(1L, 1L);
+
+        // when
+        ResponseEntity<CardPaymentDto.Response> result = PaymentController.builder()
+                .payService(testPayContainer.payService)
+                .customMapper(new CustomMapper())
+                .build()
+                .cancelPayment("testPaymentKey", "0001");
+
+        // then
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(result.getBody()).isNotNull();
+        assertThat(result.getBody().paymentStatus()).isEqualTo(PaymentStatus.CANCEL);
+        assertThat(result.getBody().cancelReason().getDescription()).isEqualTo("서비스 및 상품 불만족");
+        assertThat(result.getBody().cancelReason()).isEqualTo(PaymentCancelReason.NOT_SATISFIED_SERVICE);
+    }
+
+    @Test
+    public void cancelPaymentKey로_결제취소_실패() throws Exception {
+
+        // given
+        PaymentController.builder()
+                .payService(testPayContainer.payService)
+                .customMapper(new CustomMapper())
+                .build()
+                .keyIn(1L, 1L);
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> PaymentController.builder()
+                .payService(testPayContainer.payService)
+                .customMapper(new CustomMapper())
+                .build()
+                .cancelPayment("testPaymentKey", "0005"))
+                .isInstanceOf(ExceptionControl.NOT_MATCHED_CANCEL_STATUS.paymentException().getClass())
+                .hasMessageContaining("결제 취소사유가 올바르지 않습니다.");
     }
 }
