c3d6a732de8fb63ce080bad41dd708d47d80fcfd
swager253 <swager253@naver.com>
2023-08-23 23:08:21 +0900

    feat: clear 추가

--- src/main/java/sejong/coffee/yun/repository/redis/RedisRepository.java
+++ src/main/java/sejong/coffee/yun/repository/redis/RedisRepository.java
@@ -1,28 +1,28 @@
 package sejong.coffee.yun.repository.redis;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Duration;
 
 @Repository
+@Primary
 @RequiredArgsConstructor
 public class RedisRepository implements NoSqlRepository {
 
     private final RedisTemplate<String, String> template;
 
     @Override
-    @Transactional
     public void setValues(String key, String value) {
         ValueOperations<String, String> valueOperations = template.opsForValue();
         valueOperations.set(key, value);
     }
 
     @Override
-    @Transactional
     public void setValues(String key, String value, Duration duration) {
         ValueOperations<String, String> valueOperations = template.opsForValue();
         valueOperations.set(key, value, duration);
@@ -35,8 +35,15 @@ public class RedisRepository implements NoSqlRepository {
     }
 
     @Override
-    @Transactional
     public void deleteValues(String key) {
         template.delete(key);
+    }
+
+    @Override
+    public void clear() {
+        template.execute((RedisCallback<Object>) connection -> {
+            connection.flushDb();
+            return null;
+        });
     }
 }
