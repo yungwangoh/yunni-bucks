18a144c02e83bc9af4d398de2b4e58a7567ab845
swager253 <swager253@naver.com>
2023-08-25 16:48:18 +0900

    feat: dummy data sql add

---
+++ src/test/resources/sql/menu.sql
@@ -0,0 +1,6 @@
+insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at)
+values ( 'Bread', 1, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '커피빵', now());
+insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at)
+VALUES ( 'Beverage', 2, now(), '에티오피타 커피산', 'M', 80, 80, 80, 80, 1000, '커피', now());
+insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at)
+values ( 'Bread', 3, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '커피빵', now());
---
+++ src/test/resources/sql/truncate.sql
@@ -0,0 +1,4 @@
+set REFERENTIAL_INTEGRITY FALSE ;
+truncate table "MENU";
+truncate table "member";
+set REFERENTIAL_INTEGRITY TRUE ;
