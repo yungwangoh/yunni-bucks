99e65c85327c46251d23175de6a6f291df1c3164
swager253 <swager253@naver.com>
2023-08-25 16:44:01 +0900

    feat: 중복 로직 추가

--- src/main/java/sejong/coffee/yun/domain/exception/ExceptionControl.java
+++ src/main/java/sejong/coffee/yun/domain/exception/ExceptionControl.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 public enum ExceptionControl {
 
     // Order, member, menuList, menu
-    DUPLICATE_USER("중복되었습니다."),
+    DUPLICATE("중복되었습니다."),
     DUPLICATE_USER_NAME("이름이 중복되었습니다."),
     DUPLICATE_USER_EMAIL("이메일이 중복되었습니다."),
     NOT_MATCH_USER("아이디 혹은 비밀번호가 다릅니다."),
