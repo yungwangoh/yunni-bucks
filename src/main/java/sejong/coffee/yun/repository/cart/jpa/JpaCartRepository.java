8ddfac4cb154cb7cd07f9904141b325534cd06cb
swager253 <swager253@naver.com>
2023-08-30 18:48:48 +0900

    refactor: fetch join 추가

--- src/main/java/sejong/coffee/yun/repository/cart/jpa/JpaCartRepository.java
+++ src/main/java/sejong/coffee/yun/repository/cart/jpa/JpaCartRepository.java
@@ -1,11 +1,14 @@
 package sejong.coffee.yun.repository.cart.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import sejong.coffee.yun.domain.user.Cart;
 
 import java.util.Optional;
 
 public interface JpaCartRepository extends JpaRepository<Cart, Long> {
-    Optional<Cart> findByMemberId(Long memberId);
+    @Query("select c from Cart c join fetch c.member m where m.id = :memberId")
+    Optional<Cart> findByMemberId(@Param("memberId") Long memberId);
     boolean existsByMemberId(Long memberId);
 }
