7620825df81d316b3af3aa683101f1399924c12f
Hayoon <hy97@sju.ac.kr>
2023-09-03 18:06:56 +0900

    refactor(JpaCardRepository): date type to Optional

--- src/main/java/sejong/coffee/yun/repository/card/jpa/JpaCardRepository.java
+++ src/main/java/sejong/coffee/yun/repository/card/jpa/JpaCardRepository.java
@@ -3,8 +3,9 @@ package sejong.coffee.yun.repository.card.jpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sejong.coffee.yun.domain.user.Card;
 
+import java.util.Optional;
+
 public interface JpaCardRepository extends JpaRepository<Card, Long> {
 
-    // 카드 FK로 회원 찾기
-    Card findByMemberId(Long memberId);
+    Optional<Card> findByMemberId(Long memberId);
 }
