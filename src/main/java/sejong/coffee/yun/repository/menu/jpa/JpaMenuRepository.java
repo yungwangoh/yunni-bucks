7728a5385e1444d8da36e6b2fd7d06fbf649d293
swager253 <swager253@naver.com>
2023-10-30 16:36:26 +0900

    feat: Lock 로직 추가

--- src/main/java/sejong/coffee/yun/repository/menu/jpa/JpaMenuRepository.java
+++ src/main/java/sejong/coffee/yun/repository/menu/jpa/JpaMenuRepository.java
@@ -1,7 +1,15 @@
 package sejong.coffee.yun.repository.menu.jpa;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import sejong.coffee.yun.domain.order.menu.Menu;
 
+import javax.persistence.LockModeType;
+import java.util.Optional;
+
 public interface JpaMenuRepository extends JpaRepository<Menu, Long> {
+    @NotNull
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Menu> findById(@NotNull Long menuId);
 }
