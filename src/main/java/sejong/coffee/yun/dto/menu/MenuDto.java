2ad50f7e34174c46ef2f123ef8603132f40a6f63
swager253 <swager253@naver.com>
2023-10-31 21:23:03 +0900

    fix: dto domain 직렬화 부분 필드 동일하게 수정

--- src/main/java/sejong/coffee/yun/dto/menu/MenuDto.java
+++ src/main/java/sejong/coffee/yun/dto/menu/MenuDto.java
@@ -5,8 +5,12 @@ import sejong.coffee.yun.domain.order.menu.MenuSize;
 import sejong.coffee.yun.domain.order.menu.Nutrients;
 import sejong.coffee.yun.domain.user.Money;
 
+import java.time.LocalDateTime;
+
 public class MenuDto {
-    public record Response(Long menuId, String title, String description, Money price, Nutrients nutrients, MenuSize menuSize) {
+
+    public record Request(String title, String description, Money price, Nutrients nutrients, MenuSize menuSize, LocalDateTime now, int quantity) {}
+    public record Response(Long id, String title, String description, Money price, Nutrients nutrients, MenuSize menuSize) {
         public Response(Menu menu) {
             this(menu.getId(), menu.getTitle(), menu.getDescription(), menu.getPrice().mapBigDecimalToInt(), menu.getNutrients(), menu.getMenuSize());
         }
