6ef74712e41db56864755d226ba2637bbfb08aa9
swager253 <swager253@naver.com>
2023-10-31 14:45:20 +0900

    feat: Redis cache 설정

--- src/main/java/sejong/coffee/yun/YunApplication.java
+++ src/main/java/sejong/coffee/yun/YunApplication.java
@@ -3,7 +3,9 @@ package sejong.coffee.yun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class YunApplication {
 
