59b55d17409434f36e4763fe6a8c391b7d17f1cf
swager253 <swager253@naver.com>
2023-11-02 18:10:47 +0900

    test: 메뉴 리뷰 캐시 적용 테스트

---
+++ src/test/java/sejong/coffee/yun/service/cache/MenuReviewCacheTest.java
@@ -0,0 +1,57 @@
+package sejong.coffee.yun.service.cache;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import sejong.coffee.yun.domain.order.menu.Menu;
+import sejong.coffee.yun.domain.order.menu.MenuReview;
+import sejong.coffee.yun.domain.user.Member;
+import sejong.coffee.yun.integration.MainIntegrationTest;
+import sejong.coffee.yun.repository.menu.MenuRepository;
+import sejong.coffee.yun.repository.user.UserRepository;
+import sejong.coffee.yun.service.MenuReviewService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class MenuReviewCacheTest extends MainIntegrationTest {
+
+    @Autowired
+    private MenuRepository menuRepository;
+    @Autowired
+    private MenuReviewService menuReviewService;
+    @Autowired
+    private UserRepository userRepository;
+
+    private Menu menu;
+    private Member member;
+
+    @BeforeEach
+    void init() {
+        member = userRepository.save(member());
+        menu = menuRepository.save(bread());
+
+        int reviewCount = 100;
+        String reviewComment = "맛있어요!";
+
+        List<MenuReview> menuReviews = Stream.generate(() -> menuReviewService.create(member.getId(), menu.getId(), reviewComment, LocalDateTime.now()))
+                .limit(reviewCount)
+                .toList();
+    }
+
+    @Test
+    void 메뉴_리뷰_조회_캐시() {
+        // given
+        PageRequest pr = PageRequest.of(0, 10);
+
+        // when
+        for(int i = 0; i < 1000; i++) {
+            menuReviewService.findAllByMenuId(pr, menu.getId());
+        }
+
+        // then
+
+    }
+}
