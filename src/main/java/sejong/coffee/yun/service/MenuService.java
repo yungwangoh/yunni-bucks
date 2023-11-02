59d7174e7e085fafc0faae2038d79687fe9b9e37
swager253 <swager253@naver.com>
2023-11-02 18:07:55 +0900

    feat: 인기 메뉴 top 10 기능 추가

--- src/main/java/sejong/coffee/yun/service/MenuService.java
+++ src/main/java/sejong/coffee/yun/service/MenuService.java
@@ -3,19 +3,28 @@ package sejong.coffee.yun.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sejong.coffee.yun.domain.order.menu.Menu;
 import sejong.coffee.yun.dto.menu.MenuDto;
+import sejong.coffee.yun.dto.menu.MenuWrapperDto;
 import sejong.coffee.yun.repository.menu.MenuRepository;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import static sejong.coffee.yun.domain.exception.ExceptionControl.NOT_FOUND_MENU;
 
 @Service
 @RequiredArgsConstructor
 public class MenuService {
 
     private final MenuRepository menuRepository;
+    private final RedisTemplate<String, String> redisTemplate;
+    private static final String RANK_KEY = "rank";
 
     @Transactional
     public Menu create(Menu menu) {
@@ -27,8 +36,40 @@ public class MenuService {
         return new MenuDto.Response(menuRepository.findById(menuId));
     }
 
-    public List<MenuDto.Response> findAll() {
-        return menuRepository.findAll().stream().map(MenuDto.Response::new).toList();
+    @Cacheable(value = "menu", cacheManager = "cacheManager")
+    public MenuWrapperDto.Response findAll() {
+
+        List<MenuDto.Response> responses = menuRepository.findAll().stream().map(MenuDto.Response::new).toList();
+
+        return new MenuWrapperDto.Response(responses);
+    }
+
+    @Transactional
+    public void addPopularMenus() {
+
+        List<Menu> menus = menuRepository.findAll();
+
+        ZSetOperations<String, String> zSetOperations = redisTemplate.opsForZSet();
+
+        menus.forEach(response -> {
+            try {
+                zSetOperations.add(RANK_KEY, response.getTitle(), response.getOrderCount());
+            }
+            catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    public List<MenuDto.Response> searchPopularMenus() {
+
+        List<Menu> menus = menuRepository.findAll();
+
+        ZSetOperations<String, String> zSetOperations = redisTemplate.opsForZSet();
+
+        Set<String> rankSets = zSetOperations.reverseRange(RANK_KEY, 0, 10);
+
+        return rankSets.stream().map(rankSet -> getMenuDtoResponse(menus, rankSet)).toList();
     }
 
     @Transactional
@@ -41,5 +82,10 @@ public class MenuService {
     @CacheEvict(value = "manu", key = "#menuId", cacheManager = "cacheManager")
     public void delete(Long menuId) {
         menuRepository.delete(menuId);
+    }
+
+    private MenuDto.Response getMenuDtoResponse(List<Menu> menus, String rankSet) {
+        return menus.stream().filter(response -> Objects.equals(response.getTitle(), rankSet))
+                .findAny().map(MenuDto.Response::new).orElseThrow(NOT_FOUND_MENU::notFoundException);
     }
 }
