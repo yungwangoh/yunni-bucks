a3e837feb7353cd67c3da2ea6ccbad7072624591
swager253 <swager253@naver.com>
2023-11-02 18:05:50 +0900

    refactor: DeliveryScheduler -> SchedulerService 클래스 이름 변경

--- src/main/java/sejong/coffee/yun/infra/SchedulerService.java
+++ src/main/java/sejong/coffee/yun/infra/SchedulerService.java
@@ -6,19 +6,26 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import sejong.coffee.yun.service.DeliveryService;
+import sejong.coffee.yun.service.MenuService;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 @EnableScheduling
-public class DeliveryScheduler {
+public class SchedulerService {
 
     private final DeliveryService deliveryService;
+    private final MenuService menuService;
 
     @Scheduled(cron = "${schedules.cron.product}")
     public void processReserveScheduler() {
         log.info("schedule START, current thread = {} ", Thread.currentThread().getName());
         deliveryService.reserveDelivery();
         log.info("schedule END, current thread = {} ", Thread.currentThread().getName());
+    }
+
+    @Scheduled(cron = "0 */2 * * * *")
+    public void processAddPopularMenus() {
+        menuService.addPopularMenus();
     }
 }
