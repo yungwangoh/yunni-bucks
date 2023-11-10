5d43c5ee67141676f3fc0613a3658e34f31b44df
swager253 <swager253@naver.com>
2023-11-10 16:10:15 +0900

    refactor: 메뉴 더미 데이터 수정

--- src/test/resources/sql/menu.sql
+++ src/test/resources/sql/menu.sql
@@ -1,40 +1,40 @@
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-values ( 'Bread', 1, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '커피빵', now(), 100, 100);
+values ( 'Bread', 1, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '커피빵', now(), 100, 0);
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-VALUES ( 'Beverage', 2, now(), '에티오피타 커피산', 'M', 80, 80, 80, 80, 1000, '커피', now(), 100, 99);
+VALUES ( 'Beverage', 2, now(), '에티오피타 커피산', 'M', 80, 80, 80, 80, 1000, '커피', now(), 100, 0);
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-values ( 'Bread', 3, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '크림빵', now(), 100, 98);
+values ( 'Bread', 3, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '크림빵', now(), 100, 0);
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-values ( 'Bread', 4, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '소금빵', now(), 100, 97);
+values ( 'Bread', 4, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '소금빵', now(), 100, 0);
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-values ( 'Bread', 5, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '카레빵', now(), 100, 96);
+values ( 'Bread', 5, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '카레빵', now(), 100, 0);
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-values ( 'Bread', 6, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '호빵', now(), 100, 95);
+values ( 'Bread', 6, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '호빵', now(), 100, 0);
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-values ( 'Bread', 7, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '꽈배기', now(), 100, 94);
+values ( 'Bread', 7, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '꽈배기', now(), 100, 0);
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-values ( 'Bread', 8, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '소다빵', now(), 100, 93);
+values ( 'Bread', 8, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '소다빵', now(), 100, 0);
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-values ( 'Bread', 9, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '소라빵', now(), 100, 92);
+values ( 'Bread', 9, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '소라빵', now(), 100, 0);
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-values ( 'Bread', 10, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '소보로빵', now(), 100, 91);
+values ( 'Bread', 10, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '소보로빵', now(), 100, 0);
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-values ( 'Bread', 11, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '프레즐', now(), 100, 90);
+values ( 'Bread', 11, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '프레즐', now(), 100, 0);
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-values ( 'Bread', 12, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '스콘', now(), 100, 89);
+values ( 'Bread', 12, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '스콘', now(), 100, 0);
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-values ( 'Bread', 13, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '베이글', now(), 100, 87);
+values ( 'Bread', 13, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '베이글', now(), 100, 0);
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-values ( 'Bread', 14, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '바게트', now(), 100, 96);
+values ( 'Bread', 14, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '바게트', now(), 100, 0);
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-values ( 'Bread', 15, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '미트파이', now(), 100, 23);
+values ( 'Bread', 15, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '미트파이', now(), 100, 0);
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-values ( 'Bread', 16, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '초코 소라빵', now(), 100, 34);
+values ( 'Bread', 16, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '초코 소라빵', now(), 100, 0);
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-values ( 'Bread', 17, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '휘낭시에', now(), 100, 455);
+values ( 'Bread', 17, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '휘낭시에', now(), 100, 0);
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-values ( 'Bread', 18, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '도넛', now(), 100, 32);
+values ( 'Bread', 18, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '도넛', now(), 100, 0);
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-values ( 'Bread', 19, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '크림 베이글', now(), 100, 33);
+values ( 'Bread', 19, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '크림 베이글', now(), 100, 0);
 insert into "MENU" (dtype, id, create_at, description, menu_size, carbohydrates, fats, kcal, proteins, total_price, title, update_at, QUANTITY, ORDER_COUNT)
-values ( 'Bread', 20, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '초코 크림 베이글', now(), 100, 11);
+values ( 'Bread', 20, now(), '에티오피아 커피산', 'M', 80, 80, 80, 80, 1000, '초코 크림 베이글', now(), 100, 0);
