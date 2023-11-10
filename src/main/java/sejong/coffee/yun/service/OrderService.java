caf243225659db729541b2aa23473e125b177ec0
swager253 <swager253@naver.com>
2023-11-10 16:15:50 +0900

    refactor: 인기 메뉴 리스트 로직 전면 수정 write-back 방식 채택

--- src/main/java/sejong/coffee/yun/service/OrderService.java
+++ src/main/java/sejong/coffee/yun/service/OrderService.java
@@ -1,17 +1,23 @@
 package sejong.coffee.yun.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sejong.coffee.yun.domain.order.Calculator;
 import sejong.coffee.yun.domain.order.Order;
 import sejong.coffee.yun.domain.order.OrderPayStatus;
 import sejong.coffee.yun.domain.order.OrderStatus;
+import sejong.coffee.yun.domain.order.menu.Menu;
 import sejong.coffee.yun.domain.user.Cart;
+import sejong.coffee.yun.domain.user.CartItem;
 import sejong.coffee.yun.domain.user.Money;
 import sejong.coffee.yun.repository.cart.CartRepository;
+import sejong.coffee.yun.repository.menu.MenuRepository;
 import sejong.coffee.yun.repository.order.OrderRepository;
 
 import java.time.LocalDateTime;
@@ -19,22 +25,35 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final Calculator calculator;
     private final CartRepository cartRepository;
+    private final MenuRepository menuRepository;
+    private final RedisTemplate<String, String> redisTemplate;
+    private static final String REDIS_KEY = "rank";
 
     @Transactional
     public Order order(Long memberId, LocalDateTime now) {
 
         Cart cart = cartRepository.findByMember(memberId);
 
+        ZSetOperations<String, String> zSetOperations = redisTemplate.opsForZSet();
+
+        cart.getCartItems().stream().map(CartItem::getMenu).map(Menu::getId).map(this::addMenuOrderCountAndSubQuantity)
+                .forEach(menu -> zSetOperations.incrementScore(REDIS_KEY, menu.getTitle(), 1));
+
         Money money = calculator.calculateMenus(cart.getMember(), cart.convertToMenus());
 
         Order order = Order.createOrder(cart, money, now);
 
-        return orderRepository.save(order);
+        Order saveOrder = orderRepository.save(order);
+
+        cart.clearCartItems();
+
+        return saveOrder;
     }
 
     @Transactional
@@ -67,5 +86,14 @@ public class OrderService {
 
     public Page<Order> findAllByMemberIdAndPayStatus(Pageable pageable, Long memberId, OrderPayStatus status) {
         return orderRepository.findAllByMemberIdAndPayStatus(pageable, memberId, status);
+    }
+
+    private Menu addMenuOrderCountAndSubQuantity(Long menuId) {
+        Menu findMenu = menuRepository.findById(menuId);
+
+        findMenu.subQuantity();
+        findMenu.addOrderCount();
+
+        return findMenu;
     }
 }
